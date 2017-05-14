5d0a054a073fb3a364f455b053596d8cabcdcf9e
J Irving <j@lollyshouse.ca>
2017-05-14 19:20:31 -0400

    Add some circle config

--- project.clj
+++ project.clj
@@ -7,4 +7,6 @@
                  [duct/logger "0.1.1"]
                  [integrant "0.4.0"]
                  [org.immutant/web "2.1.6"]]
-  :profiles {:dev {:dependencies [[clj-http "3.5.0"]]}})
+  :profiles {:dev {:dependencies [[clj-http "3.5.0"]]
+                   :plugins [[test2junit "1.2.5"]]
+                   :test2junit-output-dir "target/test2junit"}})
